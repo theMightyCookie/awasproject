53aa714821eff5cde04347421a1e3d2ea7aeda53
cookie <andreea.amaria7@gmail.com>
2023-05-28 20:00:54 +0300

    deleted:    dashboard.php

--- dashboard.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-
-    // Assume $username and $password have been retrieved from the database
-    $_SESSION['username'] = $username;
-    $_SESSION['password'] = $password; // Please do not do this in a real application
-?>
